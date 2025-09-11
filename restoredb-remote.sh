0f55a930867c7b00adcfa520e721940dd7b0a6bf
Robert Verrill <robert.verrill@english.op.org>
2025-09-11 14:59:15 +0000

    restor db remote fix

--- restoredb-remote.sh
+++ restoredb-remote.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 DOCKER_FILE="docker-compose-pull.yml"
+docker compose -f $DOCKER_FILE down -v
+docker compose -f $DOCKER_FILE up -d
 docker stop aquinas-search-engine-app-1
 docker-compose -f $DOCKER_FILE exec db psql -h localhost -U rwv1001 -d postgres -c "DROP DATABASE IF EXISTS philosophyfinderpsql_production4;"
 docker-compose -f $DOCKER_FILE exec db psql -h localhost -U rwv1001 -d postgres -c "CREATE DATABASE philosophyfinderpsql_production4 OWNER rwv1001;"
